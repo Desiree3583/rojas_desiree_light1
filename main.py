54e04029bb5c3a4608c74052469ede1e4ded53f6
Desiree3583 <72775978+Desiree3583@users.noreply.github.com>
2020-12-08 11:02:04 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+while True:
+    print("Light Level:" + input.light_level())
+    if 0 < "Light Level:" + input.light_level  > 129 
+        light.clear()
+    else:
+        light.set_pixel_color(4, light.rgb(0, 0, 255))
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+while (true) {
+    console.log("Light Level:" + input.lightLevel())
+    if (0 < "Light Level:" + input.lightLevel && "Light Level:" + input.lightLevel > 129) {
+        light.clear()
+    } else {
+        light.setPixelColor(4, light.rgb(0, 0, 255))
+    }
+    
+}
